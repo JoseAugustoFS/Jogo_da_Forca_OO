e5520cf2e1ccb5c0e5adb75d05a6518ec10a76c6
JoseAugustoFS <134980478+JoseAugustoFS@users.noreply.github.com>
2023-09-10 08:40:38 -0300

    Implementação dos services.

--- Jogo da Forca OO/src/br/edu/iff/bancodepalavras/dominio/palavra/PalavraAppService.java
+++ Jogo da Forca OO/src/br/edu/iff/bancodepalavras/dominio/palavra/PalavraAppService.java
@@ -1,5 +1,47 @@
 package br.edu.iff.bancodepalavras.dominio.palavra;
 
+import br.edu.iff.bancodepalavras.dominio.tema.TemaRepository;
+import br.edu.iff.repository.RepositoryException;
+
 public class PalavraAppService {
 
+	private PalavraRepository palavraRepository;
+	private TemaRepository temaRepository;
+	private PalavraFactory palavraFactory;
+	
+	private static PalavraAppService soleInstance;
+	
+	public void createSoleInstance(PalavraRepository palavraRepository, TemaRepository temaRepository, PalavraFactory palavraFactory) {
+		if(soleInstance==null) {
+			soleInstance = new PalavraAppService(palavraRepository, temaRepository, palavraFactory);
+		}
+	}
+	
+	public static PalavraAppService getSoleInstance() {
+		return soleInstance;
+	}
+	
+	private PalavraAppService(PalavraRepository palavraRepository, TemaRepository temaRepository, PalavraFactory palavraFactory) {
+		this.palavraRepository = palavraRepository;
+		this.temaRepository = temaRepository;
+		this.palavraFactory = palavraFactory;
+	}
+	
+	public boolean novaPalavra(String palavra, long idTema) {
+		if(this.temaRepository.getPorId(idTema)==null) {
+			throw new RuntimeException("O idTema tem que ser de um Tema pré-existente no repositório de Tema.");
+		}
+		if(this.palavraRepository.getPalavra(palavra)!=null) {
+			return true;
+		}else {
+			try {
+				this.palavraRepository.inserir(this.palavraFactory.getPalavra(palavra, this.temaRepository.getPorId(idTema)));
+				return true;
+			}catch(RepositoryException e) {
+				System.out.println(e.getMessage());
+				return false;
+			}
+		}
+	}
+	
 }
